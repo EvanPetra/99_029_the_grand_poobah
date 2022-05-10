928d77baa5fd55a977df58a7c84ef76606e36386
EvanJones <evanjones@petradatascience.com>
2022-05-10 13:53:30 +1000

    lll

---
+++ 99-029-grand-poobah/poobah/src/main/java/com/petradatascience/p99029/model/components/Pile.java
@@ -0,0 +1,7 @@
+package com.petradatascience.p99029.model.components;
+
+import com.petradatascience.p99029.model.FlowComponent;
+
+public interface Pile extends FlowComponent {
+    
+}
---
+++ 99-029-grand-poobah/poobah/src/main/java/com/petradatascience/p99029/model/components/Process.java
@@ -0,0 +1,7 @@
+package com.petradatascience.p99029.model.components;
+
+import com.petradatascience.p99029.model.FlowComponent;
+
+public interface Process extends FlowComponent {
+    
+}
